07b781a1ad1540d6559f8ee419eb4b488a4f38b7
Jose-YP <joseaw2003@gmail.com>
2023-12-27 23:07:32 -0500

    Starting to make basic ranged move + more Nodes

--- Code/Player.gd
+++ Code/Player.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 @export var speed: int = 600
 @export var bowDistance: int = 600
 @export var arrow: PackedScene
+@export var trap: PackedScene
 
 @onready var bow = $RangedTools/Bow
 
 var drawingBow: bool = false
 var meleeRange: bool = false
+var stealthy: bool = false
 
 enum location {HIGH,
 	HOVERING,
@@ -23,18 +25,31 @@ func _process(delta):
 	#REGULAR CONTROL
 	var direction = Input.get_vector("Left","Right","Up","Down")
 	var aim = get_global_mouse_position() - position
+	var effectiveSpeed = speed
+	if drawingBow: #Player slows down when aiming
+		effectiveSpeed = speed * .25
 	
-	position += direction * speed * delta
+	position += direction * effectiveSpeed * delta
 	move_and_slide()
 	
 	#RANGED CONTROL
 	if not meleeRange:
 		if drawingBow:
-			if Input.is_action_just_released("Action"):
+			if Input.is_action_just_released("Action"): #When released shoot arrow
 				drawingBow = false
+				bow.hide()
+				print("Shoot ", aim)
 		else:
-			if Input.is_action_pressed("Action"):
+			if Input.is_action_pressed("Action"): #As long as action is held, keep aiming
 				drawingBow = true
 				bow.look_at(aim)
-	
-	
+				bow.show()
+				print("Looking at ", aim)
+
+func _on_melee_range_area_entered(area):
+	meleeRange = true
+	print("Entered melee range")
+
+func _on_melee_range_area_exited(area):
+	meleeRange = false
+	print("Exited melee range")
