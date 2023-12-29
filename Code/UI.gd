213523df57ac5c7805857b4dc3b25480e8bded46
Jose-YP <joseaw2003@gmail.com>
2023-12-28 22:46:10 -0500

    started work on UI and character AI

---
+++ Code/UI.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+@onready var time: Timer = $Timer
+@onready var timerText: RichTextLabel = $TimerText
+@onready var scoreText: RichTextLabel = $ScoreText
+@onready var suspectBar: TextureProgressBar = $SuspicionBar
+
+var totalTime: int = 0
+var totalScore: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	time.set_paused(false)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func tweenSuspectBar():
+	pass
+
+func riseScore():
+	totalScore += 1
+	scoreText.clear()
+	scoreText.append_text(str("Score: ", totalScore))
+
+func _on_timer_timeout():
+	totalTime += 1
+	var timeText = str("[color=green]",totalTime,"[/color]")
+	timerText.clear()
+	
+	if totalTime > 5:
+		timeText = str("[color=red]",totalTime,"[/color]")
+	
+	timerText.append_text(str("Time: ", timeText))
--- Code/character.gd
+++ Code/character.gd
@@ -1,15 +1,24 @@
 extends StaticBody2D
 
 @onready var sprite: Sprite2D = $Icon
+@onready var wanderArrays: Array[RayCast2D] = [$WanderRays/Forward,$WanderRays/Back]
+@onready var idleTimers: Array[Timer] = [$IdleTimers/ShortIdle,$IdleTimers/MidIdle,$IdleTimers/LongIdle]
 
 @export var speed: int = 20
 
 var assignedType
+var assignedMove
+var currentDirection
 
 enum type {NONE,
 	LOVER,
 	TARGETTED}
-
+enum move {WANDER,
+	SEEK,
+	LEAVE}
+enum lookDirections{FORWARD,
+	UPFORWARD,
+	UPWARD}
 #----------------------------------------------
 #INITALIZATION
 #----------------------------------------------
@@ -17,7 +26,12 @@ func _ready():
 	moreReady()
 
 func moreReady(): #Lover will inherit from Character so moreReady can be called on both scripts to keep them same
-	pass
+	for timer in idleTimers:
+		timer.set_paused(false)
+		timer.set_autostart(false)
+		timer.connect("timeout",on_idleTimer_timeout)
+	
+	assignedMove = move.WANDER
 
 #----------------------------------------------
 #PROCESSING
@@ -26,12 +40,57 @@ func _process(_delta):
 	processor()
 
 func processor(): #The same applies to processor and process
-	pass
+	match assignedMove:
+		move.WANDER:
+			var atLeastOne = false
+			for timer in idleTimers:
+				if not timer.is_stopped():
+					atLeastOne = true
+			
+			if not atLeastOne:
+				idleTimers[randi_range(0,3)].start()
+			
+		move.SEEK:
+			pass
+		move.LEAVE:
+			pass
 
 #----------------------------------------------
 #MOVING AI
 #----------------------------------------------
+func wander():
+	if $Forward/Forward.is_colliding():
+		pass
+	else:
+		pass
+
+func seek():
+	pass
+
+func leave():
+	pass
 
 #----------------------------------------------
 #OBSERVING AI
 #----------------------------------------------
+func pasiveForwardObserve():
+	pass
+
+func passiveUpForObserve():
+	pass
+
+func activeObserve():
+	pass
+
+func activeSkyObserve():
+	pass
+
+#----------------------------------------------
+#HELPER FUNTIONS
+#----------------------------------------------
+
+#----------------------------------------------
+#SIGNALS
+#----------------------------------------------
+func on_idleTimer_timeout():
+	wander()
